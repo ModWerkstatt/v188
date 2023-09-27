5c7c5529d6aec864f5527002b7a1a11f63641a08
Florian Thiel <flth@flth.de>
2023-09-27 23:47:05 +0200

    inital

---
+++ mod.lua
@@ -0,0 +1,26 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "germany", "diesel", "db", },
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,42 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "D 311 / V 188 / Class 288",
+		["mod_desc"] = "Developed for the heavy railway guns \"Dora\" and \"Gustav\", the two remaining locomotives were used for civilian purposes by the Deutschen Bundesbahn from 1950 onwards. Initially kept in bottle green, in 1958 they were equipped with new motors and industrial magnets were installed. The paint then changed to the crimson red usual for diesel locomotives. In 1969, V 188 001 was parked due to high maintenance costs, V 188 002 was parked in 1972 because of a crack in the tank. Both locomotives were scrapped in 1973.",
+        ["V188_menu"] = "D 311 / V 188 / Class 288",
+
+        ["D311-001_name"] = "D311.1 Wehrmacht (1941)",
+        ["D311-002_name"] = "D311.2 Wehrmacht (1941)",
+        ["D311_desc"] = "10 locomotive halves were commissioned to be able to move the heavy railway guns \"Dora\" and \"Gustav\". The locomotives were used in the Crimea in 1942 to move the gun and supply it with energy.",
+        ["V188-001_fg_name"] = "V 188 001 DB bottle green (1950)",
+        ["V188-002_fg_name"] = "V 188 002 DB bottle green (1950)",
+        ["V188_fg_desc"] = "The Deutsche Bundesbahn received two machines and decided to refurbish them due to the general shortage of vehicles. These were painted bottle green and designated V 188 001 a/b and V 188 002 a/b. They were used in heavy freight train service.",
+        ["V188-001_rot_name"] = "V 188 001 DB purple (1958)",
+        ["V188-002_rot_name"] = "V 188 002 DB purple (1958)",
+		["V188_rot_desc"] = "In 1957, the locomotives began to be converted to the new Maybach engines. This enabled the performance of the machines to be increased further. An Indusi magnet was also installed. V188 001 was shut down at the beginning of 1969 due to high maintenance costs.",
+		["BR288-002_name"] = "Class 288 002 DB (1968)",
+        ["BR288-002_desc"] = "The V 188 002 was renumbered in 1968 to the computer-friendly number 288 002-9. She continued to be deployed from Nuremberg. After a crack in the tank, she had to be parked in 1972 and was scrapped a year later.",
+
+	},
+	de = {
+
+		["mod_name"] = "D 311 / V 188 / BR 288",
+		["mod_desc"] = "Entwickelt für die schweren Eisenbahngeschütze \"Dora\" und \"Gustav\" wurden die zwei verbliebenen Loks bei der Deutschen Bundesbahn ab 1950 zivil genutzt. Zunächst in flaschengrün gehalten, wurden sie 1958 mit neuen Motoren ausgerüstet sowie Indusi-Magneten verbaut. Die Lackierung wechselte dann auf das für Diesellokomitiven übliche purpurrot. 1969 wurde V 188 001 wegen zu hoher Unterhaltskosten abgestellt, V 188 002 wurde 1972 wegen eines Riss im Tank abgestellt. Beide Loks wurden 1973 verschrottet.",
+        ["V188_menu"] = "D 311 / V 188 / BR 288",
+
+        ["D311-001_name"] = "D311.1 Wehrmacht (1941)",
+        ["D311-002_name"] = "D311.2 Wehrmacht (1941)",
+        ["D311_desc"] = "In Auftrag gegeben wurden 10 Lokomotivhälften um die schweren Eisenbahngeschütze \"Dora\" und \"Gustav\" bewegen zu können. Die Loks wurden auch 1942 auf der Krim eingesetzt, um das Geschützt zu bewegen und mit Energie zu versorgen.",
+        ["V188-001_fg_name"] = "V 188 001 DB flaschengrün (1950)",
+        ["V188-002_fg_name"] = "V 188 002 DB flaschengrün (1950)",
+        ["V188_fg_desc"] = "Die Deutsche Bundesbahn erhielt zwei Maschinen und entschloß sich, diese aufzuarbeiten aufgrund des allgemeinen Fahrzeugmangels. Diese wurden in flaschengrün lackiert und als V 188 001 a/b und V 188 002 a/b bezeichnet. Eingesetzt wurden sie im schweren Güterzugdienst.",
+        ["V188-001_rot_name"] = "V 188 001 DB purpurrot (1958)",
+        ["V188-002_rot_name"] = "V 188 002 DB purpurrot (1958)",
+		["V188_rot_desc"] = "1957 begann die Umrüstung der Lokomotiven auf die neuen Maybach-Motoren. Die Leistung der Maschinen konnte dadurch nochmals gesteigert werden. Außerdem wurde ein Indusi-Magnet angebaut. V188 001 wurde Anfang 1969 wegen hoher Unterhaltskosten abgestellt.",
+		["BR288-002_name"] = "BR 288 002 DB (1968)",
+        ["BR288-002_desc"] = "Die V 188 002 wurde 1968 umgezeichnet auf die EDV-gerechte Nummer 288 002-9. Sie wurde weiterhin von Nürnberg aus eingesetzt. Nach einem Riss im Tank musste sie dann 1972 abgestellt werden und wurde ein Jahr später verschrottet.",
+
+	},
+}
+end
